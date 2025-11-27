b5f9d3e39b6f1f34b23bed80a9e4129c374c418b
Ignacio Rodriguez Duran <igrd1359@gmail.com>
2025-11-27 11:21:39 -0300

    Se siguen ajustando las configuraciones para que no haya conflictos al levantar los contenedores y se puede ejecutar el workflow ci-cd

--- core_negocio/src/test/java/com/itapia/core_negocio/CoreNegocioApplicationTests.java
+++ core_negocio/src/test/java/com/itapia/core_negocio/CoreNegocioApplicationTests.java
@@ -2,8 +2,10 @@ package com.itapia.core_negocio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoreNegocioApplicationTests {
 
 	@Test
